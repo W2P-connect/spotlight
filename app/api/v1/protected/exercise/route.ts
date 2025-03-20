a11cddb7b5b933722dc1951cad632f9af8b2bb96
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-03-20 16:14:29 +0100

    updates for deploying

--- app/api/v1/protected/exercise/route.ts
+++ app/api/v1/protected/exercise/route.ts
@@ -2,6 +2,7 @@ export const dynamic = 'force-dynamic';
 
 import { NextResponse, NextRequest } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
 
 export const GET = async (req: NextRequest) => {
     try {
@@ -26,6 +27,42 @@ export const GET = async (req: NextRequest) => {
             message: 'Failed to get exercises',
             data: [],
             success: false,
+        }, { status: 500 });
+    }
+};
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const userId = req.headers.get("x-user-id") as string;
+        const exercise = await req.json();
+
+        const createdExercise = await prisma.exercise.create({
+            data: {
+                ...exercise,
+                ownerId: userId
+            }
+        });
+
+        return NextResponse.json({
+            message: 'Successfully created exercise',
+            data: createdExercise,
+            success: true,
+        })
+    } catch (error: any) {
+        console.error(error);
+
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            return NextResponse.json({
+                message: `Database error: ${error.message}`,
+                success: false,
+                data: error
+            }, { status: 500 });
+        }
+
+        return NextResponse.json({
+            message: "Failed to create exercise",
+            success: false,
+            data: error
         }, { status: 500 });
     }
 };
