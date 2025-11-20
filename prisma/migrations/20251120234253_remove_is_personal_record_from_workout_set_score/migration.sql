76f8ac1ba14dd71bd2854a70a6197d1aabb5e5ea
Tristan Dieny <tristan.d@lempire.co>
2025-11-20 23:43:41 +0100

    migration: remove is_personal_record from workout_set_score

---
+++ prisma/migrations/20251120234253_remove_is_personal_record_from_workout_set_score/migration.sql
@@ -0,0 +1,9 @@
+-- DropIndex
+DROP INDEX IF EXISTS "workout_set_score_user_id_exercise_id_is_personal_record_idx";
+
+-- AlterTable
+ALTER TABLE "workout_set_score" DROP COLUMN IF EXISTS "is_personal_record";
+
+-- CreateIndex
+CREATE INDEX IF NOT EXISTS "workout_set_score_user_id_exercise_id_idx" ON "workout_set_score"("user_id", "exercise_id");
+
